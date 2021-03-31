ece21a838a03ff5df924f99f0016410cde38df79
LucasMonastirsky <lucas.mnsk@gmail.com>
2021-03-31 12:42:52 -0300

    [Progress] Added Task model

---
+++ src/Models/Task.ts
@@ -0,0 +1,31 @@
+class Task {
+  title: string
+  status: TASK_STATUS
+  description: string
+  creator_id: string
+  completer_id?: string
+
+  constructor(properties: {
+    title?: string,
+    status?: TASK_STATUS,
+    description?: string,
+    creator_id: string,
+    completer_id?: string,
+  }){
+    this.title = properties.title ?? 'Untitled Task'
+    this.status = properties.status ?? TASK_STATUS.PENDING
+    this.description = properties.description ?? ''
+    this.creator_id = properties.creator_id
+    this.completer_id = properties.completer_id
+  }
+  
+}
+
+enum TASK_STATUS {
+  DONE = 'Done',
+  PENDING = 'Pending',
+  PAUSED = 'Paused',
+}
+
+export default Task
+export { TASK_STATUS }
---
+++ src/Models/index.ts
@@ -0,0 +1,1 @@
+export { default as Task, TASK_STATUS } from './Task'
