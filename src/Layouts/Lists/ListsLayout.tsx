a8425d81b96a64dbc5694e5cd0302034ec96c28d
LucasMonastirsky <lucas.mnsk@gmail.com>
2021-04-19 15:59:20 -0300

    [Code-Cleanup] Moved mock lists to API

--- src/Layouts/Lists/ListsLayout.tsx
+++ src/Layouts/Lists/ListsLayout.tsx
@@ -5,84 +5,16 @@ import TaskView from './TaskView'
 import { Task, TodoList } from '../../Models'
 import { colors, style } from '../../Styling'
 import ListTab from './ListTab'
-import { Navigation } from '../../App'
+import { API, Navigation } from '../../App'
 import { LayoutProps } from '../types'
 import AddTaskModal from './AddTaskModal'
 import ListEditModal from './ListEditModal'
 import ListAddModal from './ListAddModal'
-
-const debug_lists: TodoList[] = [
-  new TodoList({
-    title: 'Household',
-    description: 'Tasks for our home!',
-    id: '0',
-    member_ids: ['0', '1'],
-    tasks: [
-      new Task({title: 'Wash Dishes', creator_id: 'Laura', id: '0', position: 0}),
-      new Task({title: 'Buy Tofu', creator_id: 'Josh', id: '1', position: 1, description: `The normal kind, not the flavoured kind, it's more expensive!`}),
-      new Task({title: 'Fill Hole in the Wall', creator_id: 'Laura', id: '2', position: 2}),
-      new Task({title: 'Take Dog for a Walk', creator_id: 'Laura', id: '3', position: 3}),
-      new Task({title: 'Develop App', creator_id: 'Josh', id: '4', position: 4, description: `Yes, this one.`}),
-      new Task({title: 'I Ran out of Ideas', creator_id: 'Laura', id: '5', position: 5}),
-    ]
-  }),
-  new TodoList({
-    title: 'Vacation Trip',
-    description: 'Things to do before going on vacation',
-    id: '1',
-    member_ids: ['0', '1'],
-    tasks: [
-      new Task({title: 'Choose Destination', creator_id: 'Laura', id: '6', position: 0}),
-      new Task({title: 'Book Tickets', creator_id: 'Josh', id: '7', position: 1}),
-      new Task({title: 'Plan Activities', creator_id: 'Laura', id: '8', position: 2}),
-      new Task({title: 'Find Someone to Take Care of the Dog', creator_id: 'Laura', id: '9', position: 3}),
-      new Task({title: 'Ask for vacation at work', creator_id: 'Josh', id: '10', position: 4}),
-      new Task({title: 'I Ran out of Ideas, yet again...', creator_id: 'Laura', id: '11', position: 5}),
-    ]
-  }),
-  new TodoList({
-    title: 'Project Car',
-    description: 'An absolute money pit',
-    id: '2',
-    member_ids: ['0'],
-    tasks: [
-      new Task({title: 'Buy new jack', creator_id: 'Laura', id: '12', position: 0}),
-      new Task({title: 'Fabricate brake adaptor plate', creator_id: 'Josh', id: '13', position: 1}),
-      new Task({title: 'Buy disc brakes', creator_id: 'Laura', id: '14', position: 2}),
-      new Task({title: 'Buy fuel tank', creator_id: 'Laura', id: '15', position: 3}),
-      new Task({title: 'Build control box', creator_id: 'Josh', id: '16', position: 4}),
-      new Task({title: 'Replace alternator', creator_id: 'Laura', id: '17', position: 5}),
-    ]
-  }),
-  new TodoList({
-    title: 'Cows Rule',
-    description: 'Things to do before going on vacation',
-    id: '3',
-    member_ids: ['0', '3'],
-    tasks: [
-      new Task({title: 'They are so pretty', creator_id: 'Laura', id: '18', position: 0}),
-      new Task({title: 'In any colour', creator_id: 'Josh', id: '19', position: 1}),
-      new Task({title: 'I love them', creator_id: 'Laura', id: '20', position: 2}),
-    ]
-  }),
-  new TodoList({
-    title: 'Project Car',
-    description: 'Things to do before going on vacation',
-    id: '4',
-    member_ids: ['0', '1', '2'],
-    tasks: [
-      new Task({title: 'Choose Destination', creator_id: 'Laura', id: '6', position: 0}),
-      new Task({title: 'Book Tickets', creator_id: 'Josh', id: '7', position: 1}),
-      new Task({title: 'Plan Activities', creator_id: 'Laura', id: '8', position: 2}),
-      new Task({title: 'Find Someone to Take Care of the Dog', creator_id: 'Laura', id: '9', position: 3}),
-      new Task({title: 'Ask for vacation at work', creator_id: 'Josh', id: '10', position: 4}),
-      new Task({title: 'I Ran out of Ideas, yet again...', creator_id: 'Laura', id: '11', position: 5}),
-    ]
-  }),
-]
+import { Loading } from '../../Components'
 
 const ListLayout = (props: LayoutProps) => {
-  const [lists, setLists] = useState(debug_lists)
+  const [lists, setLists] = useState<TodoList[]>([])
+  const [getting_lists, setGettingLists] = useState(true)
   const [current_list_index, setCurrentListIndex] = useState(0)
   const [adding_task, setAddingTask] = useState(false)
   const [editting, setEditting] = useState(false)
@@ -90,6 +22,16 @@ const ListLayout = (props: LayoutProps) => {
 
   const current_list = lists[current_list_index]
 
+  useEffect(()=>{ // get lists
+    API.getCurrentUser().then(user => {
+      user.getLists().then(user_lists => {
+        setLists(user_lists)
+        setGettingLists(false)
+      })
+    })
+  }, [])
+
+  //#region Methods
   const addTask = (task: Task) => {
     const updated_list = {...current_list, tasks: [
       ...current_list.tasks,
@@ -109,7 +51,9 @@ const ListLayout = (props: LayoutProps) => {
     new_lists.splice(current_list_index, 1, list)
     setLists(new_lists)
   }
+  //#endregion
 
+  //#region Render
   useEffect(() => {
     if (props.active) Navigation.header = () => {
       const Icon = ({source, onPress}: {source: ImageSourcePropType, onPress: ()=>any}) => (
@@ -127,8 +71,15 @@ const ListLayout = (props: LayoutProps) => {
     }
   }, [props.active])
 
-  //#region Render
-  return (
+  if (getting_lists) {
+    return (
+      <View style={css.container}>
+        <Loading />
+      </View>
+    )
+  }
+
+  else return (
     <View style={css.container}>
       {adding_task && <AddTaskModal onAdd={addTask} close={()=>setAddingTask(false)} />}
       {editting && <ListEditModal list={current_list} editList={updateList} close={()=>setEditting(false)} />}
