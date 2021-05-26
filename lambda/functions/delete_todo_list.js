28b96951ca887d4acf85aa021a9c144f612b67a3
LucasMonastirsky <lucas.mnsk@gmail.com>
2021-05-26 00:57:14 -0300

    [API] Delete topics and update users after deleting list

--- lambda/functions/delete_todo_list.js
+++ lambda/functions/delete_todo_list.js
@@ -4,11 +4,50 @@ exports.handler = async (event) => {
   if (!event.id)
     throw new Error(`id parameter is missing`)
 
-  let db = new AWS.DynamoDB.DocumentClient()
-  await db.delete({
+  console.log(`List ID: ${event.id}`)
+  console.log(`Deleting list in DB...`)
+
+  const db = new AWS.DynamoDB.DocumentClient()
+  const { Attributes: list } = await db.delete({
     TableName: 'Lists',
-    Key: { id: event.id }
+    Key: { id: event.id },
+    ReturnValues: 'ALL_OLD',
   }).promise()
+
+  const member_ids = [...JSON.parse(JSON.stringify(list.member_ids))].filter(x => x !== '')
+
+  console.log(`Getting members...`)
+
+  const { Responses } = await db.batchGet({
+    RequestItems: {
+      'Users': {
+        ConsistentRead: true,
+        Keys: member_ids.map(id => ({id}))
+      }
+    }
+  }).promise()
+  const members = Responses.Users
+
+  console.log(`Updating members...`)
+
+  const mapUser = (user) => {
+    const i = user.list_ids.indexOf(list.id)
+    user.list_ids.splice(i, 1)
+    return { PutRequest: { Item: user } }
+  }
+
+  await db.batchWrite({
+    RequestItems: {
+      'Users': members.map(mapUser)
+    }
+  }).promise()
+
+  console.log(`Deleting SNS topic...`)
+
+  const sns = new AWS.SNS()
+  await sns.deleteTopic({TopicArn: list.topic_arn}).promise()
+
+  console.log(`Done!`)
 
   return {
     statusCode: 200,
